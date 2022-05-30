a1dddd1789bac961cbf1247cf9f4bb87a9fee0c6
Felype Ganzert <felipe.ganzert@gmail.com>
2022-05-30 00:23:32 -0300

    Created Spring Boot project

---
+++ src/main/java/com/felypeganzert/controlelivroapi/ControleLivroApiApplication.java
@@ -0,0 +1,13 @@
+package com.felypeganzert.controlelivroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleLivroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleLivroApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felypeganzert/controlelivroapi/ControleLivroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felypeganzert.controlelivroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleLivroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
