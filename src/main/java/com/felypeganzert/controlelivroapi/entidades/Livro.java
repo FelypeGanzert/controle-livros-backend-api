8707932ee349e3333c09176b13b5cafcc9eb870a
Felype Ganzert <felipe.ganzert@gmail.com>
2022-05-30 00:25:01 -0300

    Cria a entidade Livro

---
+++ src/main/java/com/felypeganzert/controlelivroapi/entidades/Livro.java
@@ -0,0 +1,42 @@
+package com.felypeganzert.controlelivroapi.entidades;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_livro")
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private String autor;
+
+    @Column(nullable = false)
+    private int paginas;
+    
+    private String descricao;
+
+    private String isbn10;
+    private String isbn13;
+
+}
